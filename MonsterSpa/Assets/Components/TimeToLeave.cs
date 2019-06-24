4651ff9f1f451f22faa3006f17e646470cf61212
Erik Sooter <erikso@unity3d.com>
2019-06-24 14:58:38 +0200

    Adding time to leave component and decrement time system for use on monsters

---
+++ MonsterSpa/Assets/Components/TimeToLeave.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[Serializable]
+public struct TimeToLeave : IComponentData
+{
+    public float TimeRemaining; //in seconds
+    
+    
+}
---
+++ MonsterSpa/Assets/Systems/DecrementTime.cs
@@ -0,0 +1,38 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using static Unity.Mathematics.math;
+using UnityEngine;
+
+public class DecrementTime : JobComponentSystem
+{
+    
+    [BurstCompile]
+    struct FatherTimeJob : IJobForEach<TimeToLeave>
+    {
+        public float dt;
+         
+        public void Execute(ref TimeToLeave timeToLeave)
+        {
+            timeToLeave.TimeRemaining -= dt;
+            if (timeToLeave.TimeRemaining < 0)
+                timeToLeave.TimeRemaining = 0;
+
+
+        }
+    }
+    
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
+    {
+        var job = new FatherTimeJob()
+        {
+            dt = Time.deltaTime
+        }.Schedule(this, inputDependencies);
+
+        return job;
+
+    }
+}
