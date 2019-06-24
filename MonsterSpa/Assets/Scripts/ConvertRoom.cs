45747b69ae935f6bdb2c598137b42caba4289473
Erik Sooter <erikso@unity3d.com>
2019-06-24 17:22:33 +0200

    Adding room components and room converter.

---
+++ MonsterSpa/Assets/Scripts/ConvertRoom.cs
@@ -0,0 +1,23 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+[RequiresEntityConversion]
+public class ConvertRoom : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public void Convert(Entity entity, EntityManager entityManager, GameObjectConversionSystem conversionSystem)
+    {
+        var roomTag = new Tag_Room();
+
+        var temp = new Temperature();
+        temp.Value = 100; //do some changes based on what room we are in!
+        
+        var name = new RoomName();
+
+        name.name = new NativeString64(gameObject.name);
+
+        entityManager.AddComponentData(entity, roomTag);
+        entityManager.AddComponentData(entity, temp);
+        entityManager.AddComponentData(entity, name);
+
+    }
+}
---
+++ MonsterSpa/Assets/Scripts/GameMgr.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+using System;
+using UnityEngine.UIElements;
+
+public class GameMgr : MonoBehaviour
+{
+    public static GameMgr g;
+    public NativeArray<Entity> rooms;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(g != null)
+            throw new Exception("Multiple GameMgr gameobject components in scene, please only have one constant gameobject in the scene.");
+
+        g = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
