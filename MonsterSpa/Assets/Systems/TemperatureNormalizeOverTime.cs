d63e70568302d6b1b1d3827f1cb79efabb1f676c
Erik Sooter <erikso@unity3d.com>
2019-06-25 18:23:24 +0200

    Fixing texture on the chick objects

--- MonsterSpa/Assets/Systems/TemperatureNormalizeOverTime.cs
+++ MonsterSpa/Assets/Systems/TemperatureNormalizeOverTime.cs
@@ -8,52 +8,52 @@ using Unity.Transforms;
 using UnityEngine;
 using static Unity.Mathematics.math;
 
-public class TemperatureNormalizeOverTime : JobComponentSystem
-{
+//public class TemperatureNormalizeOverTime : JobComponentSystem
+//{
 
-    [BurstCompile]
-    private struct TemperatureNormalizeOverTimeJob : IJobForEach<Temperature, InsideRoom>
-    {
-        public float TemperatureNormalizeRate;
-        public float dt;
-
-        [ReadOnly] public ComponentDataFromEntity<RoomTemperature> RoomTemperature;
-
-        public void Execute(ref Temperature temperature, [ReadOnly]ref InsideRoom room)
-        {
-            var roomTemperature = RoomTemperature[room.RoomEntity].Value;
-            
-            if (temperature.Value > roomTemperature)
-            {
-                temperature.Value -= TemperatureNormalizeRate * dt;
-
-                if (temperature.Value < roomTemperature)
-                {
-                    temperature.Value = roomTemperature;
-                }
-            }
-            else
-            {
-                temperature.Value += TemperatureNormalizeRate * dt;
-
-                if (temperature.Value > roomTemperature)
-                {
-                    temperature.Value = roomTemperature;
-                }
-
-            }
-        }
-    }
-    
-    protected override JobHandle OnUpdate(JobHandle inputDependencies)
-    {
-        var job = new TemperatureNormalizeOverTimeJob()
-        {
-            TemperatureNormalizeRate = Constants.g.TemperatureNormalizeRate,
-            dt = Time.deltaTime,
-            RoomTemperature = GetComponentDataFromEntity<RoomTemperature>(true)
-        };
-        
-        return job.Schedule(this, inputDependencies);
-    }
-}
+    //Ω≈å≈ç∂√ƒ®[BurstCompile]
+    //ßprivate struct TemperatureNormalizeOverTimeJob : IJobForEach<Temperature, InsideRoom>
+    //ß{
+    //ß    public float TemperatureNormalizeRate;
+    //ß    public float dt;
+//ß
+    //ß    [ReadOnly] public ComponentDataFromEntity<RoomTemperature> RoomTemperature;
+//ß
+    //ß    public void Execute(ref Temperature temperature, [ReadOnly]ref InsideRoom room)
+    //ß    {
+    //ß        //var roomTemperature = RoomTemperature[room.RoomEntity].Value;
+    //ß        //
+    //ß        //if (temperature.Value > roomTemperature)
+    //ß        //{
+    //ß        //    temperature.Value -= TemperatureNormalizeRate * dt;
+////ß
+    //ß        //    if (temperature.Value < roomTemperature)
+    //ß        //    {
+    //ß        //        temperature.Value = roomTemperature;
+    //ß        //    }
+    //ß        //}
+    //ß        //else
+    //ß        //{
+    //ß        //    temperature.Value += TemperatureNormalizeRate * dt;
+////ß
+    //ß        //    if (temperature.Value > roomTemperature)
+    //ß        //    {
+    //ß        //        temperature.Value = roomTemperature;
+    //ß        //    }
+////ß
+    //ß        //}
+    //ß    }
+    //ß}
+    //ß
+    //ßprotected override JobHandle OnUpdate(JobHandle inputDependencies)
+    //ß{
+    //ß    //var job = new TemperatureNormalizeOverTimeJob()
+    //ß    //{
+    //ß    //    TemperatureNormalizeRate = Constants.g.TemperatureNormalizeRate,
+    //ß    //    dt = Time.deltaTime,
+    //ß    //    RoomTemperature = GetComponentDataFromEntity<RoomTemperature>(true)
+    //ß    //};
+    //ß    //
+    //ß    //return job.Schedule(this, inputDependencies);
+    //ß}
+//ß}
