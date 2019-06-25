fd5cc7191d989e3f7dc65267fc76294596ab3562
Erik Sooter <erikso@unity3d.com>
2019-06-25 17:54:12 +0200

    Updating prefabs, moving monsters to the room they spawn in. spawning 2 monsters per second for some reason...

--- MonsterSpa/Assets/Scripts/GameMgr.cs
+++ MonsterSpa/Assets/Scripts/GameMgr.cs
@@ -4,24 +4,33 @@ using Unity.Collections;
 using Unity.Entities;
 using UnityEngine;
 using System;
+using Unity.Transforms;
 using UnityEngine.UIElements;
 
 public class GameMgr : MonoBehaviour
 {
     public static GameMgr g;
     public List<Entity> rooms = new List<Entity>();
-    public List<Entity> monsters = new List<Entity>();
+    public static List<Entity> monsters = new List<Entity>();
     public GameObject lobby;
     public GameObject sauna;
     public GameObject hotTub;
     public GameObject coldBath;
     public GameObject cafe;
-    public GameObject monster;
-    
-    
+    public GameObject Chick;
+    public GameObject Ghost;
+    public GameObject Sandal;
+    public GameObject Hundun;
+
+    public static List<Entity> monsterEnts = new List<Entity>();
+
+    public int spawnrate; //in seconds
+
+    private float starttime;
     // Start is called before the first frame update
     void Start()
     {
+        starttime = Time.time;
         if(g != null)
             throw new Exception("Multiple GameMgr gameobject components in scene, please only have one constant gameobject in the scene.");
 
@@ -45,16 +54,38 @@ public class GameMgr : MonoBehaviour
         
         
         //monsters should just come inside over time
-        var monEnt =  GameObjectConversionUtility.ConvertGameObjectHierarchy(monster, World.Active);
-        monsters.Add(entityManager.Instantiate(monEnt));
+        var monEnt =  GameObjectConversionUtility.ConvertGameObjectHierarchy(Chick, World.Active);
+        monsterEnts.Add(monEnt);
+        monEnt =  GameObjectConversionUtility.ConvertGameObjectHierarchy(Ghost, World.Active);
+        monsterEnts.Add(monEnt);
+        monEnt =  GameObjectConversionUtility.ConvertGameObjectHierarchy(Sandal, World.Active);
+        monsterEnts.Add(monEnt);
+        monEnt =  GameObjectConversionUtility.ConvertGameObjectHierarchy(Hundun, World.Active);
+        monsterEnts.Add(monEnt);
+        
+        
+        monsters.Add(entityManager.Instantiate(monsterEnts[0]));
+        
         
         entityManager.SetComponentData(monsters[0], new InsideRoom(){RoomEntity = rooms[0]}); //room 0 is lobby!
+        //entityManager.SetComponentData();
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Time.time % spawnrate < 0.1)
+        {
+            EntityManager entityManager = World.Active.EntityManager;
         
+            //spawn a new monster!
+            var monsterEnt = GameObjectConversionUtility.ConvertGameObjectHierarchy(Chick, World.Active);
+            var thing = entityManager.Instantiate((monsterEnt));
+            entityManager.SetComponentData(thing, new Translation(){Value=sauna.transform.position});
+            monsters.Add(entityManager.Instantiate(monsterEnt));
+            
+            entityManager.SetComponentData(monsterEnt, new InsideRoom(){RoomEntity = rooms[0]}); //room 0 is lobby!
+        }
     }
 }
--- MonsterSpa/Assets/Systems/MoveToExit.cs
+++ MonsterSpa/Assets/Systems/MoveToExit.cs
@@ -8,6 +8,7 @@ using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
 using static Unity.Mathematics.math;
+using Quaternion = UnityEngine.Quaternion;
 using Vector3 = UnityEngine.Vector3;
 
 public class MoveToExit : JobComponentSystem
@@ -27,34 +28,21 @@ public class MoveToExit : JobComponentSystem
         // For example,
         //    public float deltaTime;
 
-        public float dt;
         public float monsterspeed;
         public EntityCommandBuffer.Concurrent ecb;
+        //index is the index of this system that is executing, entity is the entity that is being executed on.
         public void Execute(Entity entity, int index, ref Translation translation, ref Rotation rotation, [ReadOnly] ref TimeToLeave timeToLeave)
         {
-            // Implement the work to perform for each entity here.
-            // You should only access data that is local or that is a
-            // field on this job. Note that the 'rotation' parameter is
-            // marked as [ReadOnly], which means it cannot be modified,
-            // but allows this job to run in parallel with other jobs
-            // that want to read Rotation component data.
-            // For example,
-            //     translation.Value += mul(rotation.Value, new float3(0, 0, 1)) * deltaTime;
-            
-            
-            
             if (timeToLeave.TimeRemaining < 0.01)
             {
-                //
-                //var directionToExit = deltaV.normalized;
                 var directionToExit = normalize(StaticExit.exitPos - translation.Value);
+                rotation.Value = Quaternion.LookRotation(directionToExit);//math.quaternion(directionToExit)  directionToExit
                 translation.Value = directionToExit * monsterspeed + translation.Value;
                 
                 var deltaV = StaticExit.exitPos - translation.Value;
                 if (math.length(deltaV) < 0.1f)
                 {
-                    //destroy the object
-                    //EndSimulationEntityCommandBufferSystem
+                    GameMgr.monsters.Remove(entity);
                     ecb.DestroyEntity(index,entity);
                 }
 
@@ -67,7 +55,7 @@ public class MoveToExit : JobComponentSystem
 
     protected override void OnCreate()
     {
-        //ecb is how we do actions on object
+        //ecb is how we do actions on object currently running this job
         endSimulationECB = World.Active.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>(); //here we define which ECB to run on
         base.OnCreate();
     }
@@ -76,7 +64,6 @@ public class MoveToExit : JobComponentSystem
     {
         var job = new MoveToExitJob()
         {
-            dt = Time.deltaTime,
             monsterspeed = 0.01f,
             ecb = endSimulationECB.CreateCommandBuffer().ToConcurrent()
         }.Schedule(this, inputDependencies);
