768ab2c671c0a6e914b1365e87af6efe55f3565b
luna-meier-unity <luna.meier@unity3d.com>
2019-06-26 13:14:06 +0200

    Audio Commit Lets Go

---
+++ MonsterSpa/Assets/SoundEffect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffect : MonoBehaviour
+{
+    public float pitchMin;
+    public float pitchMax;
+
+
+    private AudioSource audioSrc;
+
+    void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.M))
+            Play();
+    }
+
+    public void Play()
+    {
+        audioSrc.pitch = Random.Range(pitchMin, pitchMax);
+        audioSrc.Play();
+    }
+}
